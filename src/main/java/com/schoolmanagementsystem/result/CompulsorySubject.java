867abfc59f05b8b1abc39bf0f158e3e59d6cc5be
Nirjhar <nirjharsingha@gmail.com>
2023-05-18 18:29:40 +0600

    project report added

--- src/main/java/com/schoolmanagementsystem/result/CompulsorySubject.java
+++ src/main/java/com/schoolmanagementsystem/result/CompulsorySubject.java
@@ -1,23 +1,34 @@
 package com.schoolmanagementsystem.result;
 
+import com.schoolmanagementsystem.controllers.ClubController;
+import com.schoolmanagementsystem.database.ConnectDatabase;
 import com.schoolmanagementsystem.users.Student;
 
-public class CompulsorySubject extends Student {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class CompulsorySubject implements Subject{
     private double Ban1;
     private double Ban2;
     private double Eng1;
     private double Eng2;
 
+    private int Class;
+    private int teacherID;
+
     public CompulsorySubject() {
         super();
     }
 
-    public CompulsorySubject(int id, double Ban1, double Ban2, double Eng1, double Eng2) {
+    public CompulsorySubject(int Class, double Ban1, double Ban2, double Eng1, double Eng2) {
         this.Ban1 = Ban1;
         this.Ban2 = Ban2;
         this.Eng1 = Eng1;
         this.Eng2 = Eng2;
-
+        this.Class = Class;
     }
 
     public void setBan1(double Ban1) {
@@ -50,5 +61,30 @@ public class CompulsorySubject extends Student {
 
     public double getEng2() {
         return Eng2;
+    }
+
+    @Override
+    public ArrayList<Integer> getStudents() throws SQLException {
+        ConnectDatabase db = new ConnectDatabase();
+        Connection con = db.getCon();
+
+        String query = "SELECT studentID FROM studentName WHERE class = ?";
+        PreparedStatement statement = con.prepareStatement(query);
+        statement.setInt(1, this.Class);
+        ResultSet r = statement.executeQuery();
+
+        ArrayList<Integer> records = new ArrayList<>();
+
+        while (r.next()) {
+            int record = r.getInt("studentID");
+            records.add(record);
+        }
+
+        return records;
+    }
+
+    @Override
+    public int getTeacher() {
+        return this.teacherID;
     }
 }
